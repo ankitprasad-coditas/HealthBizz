e822d4be55b3512140d41b1cf1a86e5074149c3d
ankit.prasad-coditas <ankit.prasad@coditas.com>
2024-12-09 23:40:44 +0530

    User Controller

--- src/main/java/com/HealthBizz/Survey/controller/UserController.java
+++ src/main/java/com/HealthBizz/Survey/controller/UserController.java
@@ -3,6 +3,11 @@ package com.HealthBizz.Survey.controller;
 import com.HealthBizz.Survey.dto.ApiResponseDto;
 import com.HealthBizz.Survey.dto.AuthRequestDto;
 import com.HealthBizz.Survey.dto.TokenResponseDto;
+import com.HealthBizz.Survey.dto.UserDto;
+import com.HealthBizz.Survey.service.AuthService;
+import com.HealthBizz.Survey.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/user")
 public class UserController {
 
+    private final AuthService authService;
+    private final UserService userService;
+
+    @Autowired
+    public UserController(AuthService authService, UserService userService) {
+        this.authService = authService;
+        this.userService = userService;
+    }
+
     @PostMapping("/login")
     public ResponseEntity<ApiResponseDto<TokenResponseDto>> userLogin(@RequestBody AuthRequestDto authRequestDto){
+        ApiResponseDto<TokenResponseDto> response = new ApiResponseDto<>(authService.login(authRequestDto), HttpStatus.OK.value(), "Logged In Successfully");
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
 
+    @PostMapping("/createUser")
+    public ResponseEntity<ApiResponseDto<UserDto>> createUser(@RequestBody UserDto userDto){
+        ApiResponseDto<UserDto> response = new ApiResponseDto<>(userService.createUser(userDto),HttpStatus.CREATED.value(), "User Created Successfully");
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
     }
 
 }
